87e8c3cc02c2bccce489cd3bcbd9aa4eb6b5d841
Phutawan Chanrueng <phutawanchanrueng@gmail.com>
2024-02-01 17:30:01 +0700

    Add "Loading icon"
    and fix the thumbnail for better responsive.

--- super-app/web/src/app/markets/components/marketsearchbox.jsx
+++ super-app/web/src/app/markets/components/marketsearchbox.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import axios from 'axios';
 import { BsCart4 } from "react-icons/bs";
-// import Image from "next/image";
+import { RiLoader4Line } from "react-icons/ri";
 import Link from "next/link";
 import "./style.css";
 
@@ -9,21 +9,31 @@ export default function MarketSearchBox() {
     const [search, setSearch] = useState("");
     const [results, setResults] = useState([]);
     const [showResults, setShowResults] = useState(false);
+    const [loading, setLoading] = useState(false);
 
     const handleSearch = (e) => {
         const searchTerm = e.target.value;
         setSearch(searchTerm);
+        setShowResults(searchTerm !== "");
+
+        if (!searchTerm) {
+            setResults([]);
+            setLoading(false);
+        }
     }
 
     useEffect(() => {
-        const delayDebounceFn = setTimeout(() => {
-        console.log(search)
-        fetchList(search);
-        setShowResults(search !== "");
-    }, 1000)
+        if (search && !loading) {
+            setLoading(true);
+            const delayDebounceFn = setTimeout(() => {
+                fetchList(search);
+            }, 1000);
 
-    return () => clearTimeout(delayDebounceFn)
-    }, [search])
+            return () => {
+                clearTimeout(delayDebounceFn);
+            };
+        }
+    }, [search]);
 
     const fetchList = (searchTerm) => {
         axios.get(`https://65bb214bb4d53c0665540e31.mockapi.io/api/v1/searchlist`)
@@ -35,8 +45,10 @@ export default function MarketSearchBox() {
             })
             .catch(err => {
                 console.log(err);
+            })
+            .finally(() => {
+                setLoading(false);
             });
-        // console.log(results)
     }
 
     return (
@@ -55,17 +67,22 @@ export default function MarketSearchBox() {
                 </div>
             </div>
             <div className={`search_results ${showResults ? 'show' : ''}`}>
-                {results.map(result => (
-                    <Link href={result.url} key={result.id}>
-                        <div className="search_result_item">
-                            <div className="search_result_item_title">
-                                {result.name}
-                            </div>
-                            <div>
-                                <img className="search_result_item_image" src={result.image} alt={result.name}/>
-                            </div>
+                {loading && (
+                    <div className="loading_spinner">
+                        <RiLoader4Line className="spinner_icon" />
+                    </div>
+                )}
+                {!loading && results.map(result => (
+                <Link href={result.url} key={result.id}>
+                    <div className="search_result_item">
+                        <div className="search_result_item_title">
+                            {result.name}
                         </div>
-                    </Link>
+                        <div>
+                            <img className="search_result_item_image" src={result.image} alt={result.name}/>
+                        </div>
+                    </div>
+                </Link>
                 ))}
             </div>
         </main>
