ddcd900ea6cd4f4ca91aeba2d2f613c122aa64fd
Buratud Thumboon <buratud@gmail.com>
2024-02-03 00:32:08 +0700

    Wrong variable again

--- env.sh
+++ env.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 PUSH_PATTERN='^(refs/heads/)(.+)'
 PULL_PATTERN='^(refs/pull/)(.+)(/merge)'
-GIT_SHA=$(echo $GITHUB_SHA | sed -r "s|$PUSH_PATTERN|\2|g")
-GIT_SHA=$(echo $GIT_SHA | sed -r "s|$PULL_PATTERN|\2|g")
-GIT_SHA=$(echo $GIT_SHA | sed -r "s|/|-|g")
-echo "GIT_SHA=$GIT_SHA"
-export GIT_SHA=$GIT_SHA
+GIT_REF=$(echo $GITHUB_SHA | sed -r "s|$PUSH_PATTERN|\2|g")
+GIT_REF=$(echo $GIT_REF | sed -r "s|$PULL_PATTERN|\2|g")
+GIT_REF=$(echo $GIT_REF | sed -r "s|/|-|g")
+echo "GIT_REF=$GIT_REF"
