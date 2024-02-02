5ece199e90e266eeb0d67921fced23b783459b4a
Buratud Thumboon <buratud@gmail.com>
2024-02-02 16:34:28 +0700

    Add Hello World Test

--- super-app/server/main.js
+++ super-app/server/main.js
@@ -3,6 +3,10 @@ const { PORT } = require('./config');
 
 const app = express();
 
+app.get('/', (req, res) => {
+  res.send('Hello World');
+});
+
 app.listen(PORT, () => {
   console.log(`Server is listening on port ${PORT}`);
 });
