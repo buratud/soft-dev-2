d06fd3b39b89370791e63b503c19ae9db08461fb
Buratud Thumboon <buratud@gmail.com>
2024-02-03 01:17:05 +0700

    Try use `BASE_SERVER_PATH` in superapp server

--- super-app/server/main.js
+++ super-app/server/main.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const { PORT } = require('./config');
-
+const { BASE_SERVER_PATH } = require('./config');
 const app = express();
 const api = express.Router();
-app.use('/api', api)
+app.use(BASE_SERVER_PATH, api)
 
 api.get('/', (req, res) => {
-  res.send('Hello World');
+  res.send(JSON.stringify(req));
 });
 
 app.listen(PORT, () => {
