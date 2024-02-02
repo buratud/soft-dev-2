2fc042ddad294ab88fcae16ec3ab2bc953c67292
Buratud Thumboon <buratud@gmail.com>
2024-02-03 01:46:05 +0700

    Add build args

--- env.sh
+++ env.sh
@@ -5,3 +5,10 @@ GIT_REF=$(echo $GITHUB_REF | sed -r "s|$PUSH_PATTERN|\2|g")
 GIT_REF=$(echo $GIT_REF | sed -r "s|$PULL_PATTERN|\2|g")
 GIT_REF=$(echo $GIT_REF | sed -r "s|/|-|g")
 echo "GIT_REF=$GIT_REF"
+
+if (GIT_REF == "main")
+then
+  echo 'BASE_SUPERAPP_WEB_PATH=""'
+else
+  echo "BASE_SUPERAPP_WEB_PATH=/$GIT_REF"
+fi
