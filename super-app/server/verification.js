cad9b7b1919d3ddbc8a69d14e9ea81e48094b9f4
KeneticKills <109983145+KeneticKills@users.noreply.github.com>
2024-02-02 21:24:18 +0700

    Update Verification

--- super-app/server/verification.js
+++ super-app/server/verification.js
@@ -1,39 +1,37 @@
 const express = require('express');
 const { createClient } = require('@supabase/supabase-js');
+const port = require('./config').PORT;
 
 const app = express();
-const port = 3001;
 
-const SUPABASE_URL = "https://nypzyitcvjrnisjdsbpk.supabase.co"
-const SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im55cHp5aXRjdmpybmlzamRzYnBrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDY3MTY3MTYsImV4cCI6MjAyMjI5MjcxNn0.MomIyp4TbTPZqADXu1CeU9pyw3DDy9zsySSFZXUy_kY"
+const SUPABASE_URL = "https://nypzyitcvjrnisjdsbpk.supabase.co";
+const SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im55cHp5aXRjdmpybmlzamRzYnBrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDY3MTY3MTYsImV4cCI6MjAyMjI5MjcxNn0.MomIyp4TbTPZqADXu1CeU9pyw3DDy9zsySSFZXUy_kY";
 const supabase = createClient(SUPABASE_URL, SUPABASE_KEY);
 
 
 
 app.use(express.json());
 
-  app.post('/verify-otp', async (req, res) => {
-    try {
-      const email = req.body.email;
-      const otp = req.body.otp;
-      console.log(`email : ${email} , otp : ${otp}`);
+app.put('/verify-otp', async (req, res) => {
+  try {
+    const {email,otp} = req.body;
   
-      // Verify the OTP
-      const { error } = await supabase.auth.verifyOtp({
-        email,
-        token: otp,
-        type: 'email'
-      });
+    // Verify the OTP
+    const { error } = await supabase.auth.verifyOtp({
+      email,
+      token: otp,
+      type: 'email'
+    });
   
-      if (error) {
-        throw error;
-      }
-  
-      res.status(200).json({ message: 'OTP verified successfully' });
-    } catch (error) {
-      console.error(error);
-      res.status(500).json({ error: 'Internal Server Error' });
+    if (error) {
+      throw error;
     }
-  });
+
+    res.status(200).json({ message: 'OTP verified successfully' });
+
+  } catch (error) {
+    res.status(500).json({ error: 'OTP has expired or is invalid ' });
+  }
+});
   
 app.listen(port, () => console.log(`Server is running on port ${port}`));
