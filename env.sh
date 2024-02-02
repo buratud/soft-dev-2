749a188788fa6f1e30a3dbc6b27a910e373e3719
Buratud Thumboon <buratud@gmail.com>
2024-02-03 00:10:32 +0700

    Forget to remove dollar sign on variable assignment

--- env.sh
+++ env.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 PUSH_PATTERN='^(refs/heads/)(.+)'
 PULL_PATTERN='^(refs/pull/)(.+)(/merge)'
-$GIT_SHA=$(echo $GITHUB_REF | sed -r "s|$PUSH_PATTERN|\2|g")
-$GIT_SHA=$(echo $GIT_SHA | sed -r "s|$PULL_PATTERN|\2|g")
-$GIT_SHA=$(echo $GIT_SHA | sed -r "s|/|-|g")
+GIT_SHA=$(echo $GITHUB_SHA | sed -r "s|$PUSH_PATTERN|\2|g")
+GIT_SHA=$(echo $GIT_SHA | sed -r "s|$PULL_PATTERN|\2|g")
+GIT_SHA=$(echo $GIT_SHA | sed -r "s|/|-|g")
 echo "GIT_SHA=$GIT_SHA"
 export GIT_SHA=$GIT_SHA
