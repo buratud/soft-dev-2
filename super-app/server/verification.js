34d0d878cc9188a67d78d6e505e3ee3d7238d67d
KeneticKills <109983145+KeneticKills@users.noreply.github.com>
2024-02-02 17:14:31 +0700

    add Verification Check

---
+++ super-app/server/verification.js
@@ -0,0 +1,45 @@
+const express = require('express');
+const { createClient } = require('@supabase/supabase-js');
+
+const app = express();
+const port = 3001;
+
+const SUPABASE_URL = "https://nypzyitcvjrnisjdsbpk.supabase.co"
+const SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im55cHp5aXRjdmpybmlzamRzYnBrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDY3MTY3MTYsImV4cCI6MjAyMjI5MjcxNn0.MomIyp4TbTPZqADXu1CeU9pyw3DDy9zsySSFZXUy_kY"
+const supabase = createClient(SUPABASE_URL, SUPABASE_KEY);
+
+
+
+app.use(express.json());
+
+  app.put('/verify-otp', async (req, res) => {
+    try {
+      const email = req.body.email;
+      const otp = req.body.otp;
+      console.log('------');
+      console.log('------');
+      console.log('------');
+      console.log('------');
+      console.log('------');
+      console.log(`email : ${email} , otp : ${otp}`);
+      console.log(req);
+  
+      // Verify the OTP
+      const { error } = await supabase.auth.verifyOtp({
+        email,
+        token: otp,
+        type: 'email'
+      });
+  
+      if (error) {
+        throw error;
+      }
+  
+      res.status(200).json({ message: 'OTP verified successfully' });
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ error: 'Internal Server Error' });
+    }
+  });
+  
+app.listen(port, () => console.log(`Server is running on port ${port}`));
