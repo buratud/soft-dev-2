2494e1e0795bff559aa744ad0c8feb7507c7b3dd
Buratud Thumboon <buratud@gmail.com>
2024-02-03 13:23:44 +0700

    Change workflow trigger event

--- env.sh
+++ env.sh
@@ -6,16 +6,18 @@ GIT_REF=$(echo $GIT_REF | sed -r "s|$PULL_PATTERN|\2|g")
 GIT_REF=$(echo $GIT_REF | sed -r "s|/|-|g")
 echo "GIT_REF=$GIT_REF"
 
-if (GIT_REF == "main")
-then
+BASE_PATH=$([ "$GIT_REF" == "main" ] && echo "" || echo "/$GIT_REF")
+
+if [ $GIT_REF == "main" ]; then
   echo 'BASE_SUPERAPP_WEB_PATH=""'
 else
   echo "BASE_SUPERAPP_WEB_PATH=/$GIT_REF"
 fi
 
-if (GIT_REF == "main")
-then
+if [ $GIT_REF == "main" ]; then
   echo 'BASE_EATS_WEB_PATH=/eats'
 else
   echo "BASE_EATS_WEB_PATH=/$GIT_REF/eats"
 fi
+
+echo "MAIN_URL=https://linux-vm-southeastasia-2.southeastasia.cloudapp.azure.com$BASE_PATH"
