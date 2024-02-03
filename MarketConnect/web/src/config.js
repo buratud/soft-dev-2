6cd598dfbfa435dc87e70e919bfae66ce4756dcf
Buratud Thumboon <buratud@gmail.com>
2024-02-03 21:22:53 +0700

    Edit `config.js`

--- MarketConnect/web/src/config.js
+++ MarketConnect/web/src/config.js
@@ -1,4 +1,7 @@
-export const baseApiUrl = 'http://localhost:5000';
-export const supabaseUrl = process.env.REACT_APP_SUPABASE_URL;
-export const supabaseKey = process.env.REACT_APP_SUPABASE_ANON_KEY;
-export const baseWebUrl = 'http://localhost:3000';
+const BASE_DOMAIN_WITH_PROTOCOL = process.env.BASE_DOMAIN_WITH_PROTOCOL;
+const BASE_SERVER_PATH = process.env.BASE_SERVER_PATH || '/api/markets';
+const BASE_WEB_PATH = process.env.BASE_WEB_PATH || '/markets';
+export const SUPABASE_URL = process.env.SUPABASE_URL;
+export const SUPABASE_ANON_KEY = process.env.SUPABASE_ANON_KEY;
+export const BASE_WEB_URL = `${BASE_DOMAIN_WITH_PROTOCOL}${BASE_WEB_PATH}`;
+export const BASE_API_URL = `${BASE_DOMAIN_WITH_PROTOCOL}${BASE_SERVER_PATH}`;
