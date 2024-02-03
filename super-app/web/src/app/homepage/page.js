72b56ff9ef1e6bd00142d241cdcadc1d465692f0
krit9354 <krit9354@gmail.com>
2024-02-03 18:44:22 +0700

    final_portal

--- super-app/web/src/app/homepage/page.js
+++ super-app/web/src/app/homepage/page.js
@@ -1,14 +1,35 @@
 'use client'
 import Link from "next/link";
-
+import style from "./page.module.css";
 
 export default function Home() {
     return (
-        <div>
-            <Link href='hornaidee/index.html'>hornaidee</Link>
-            <Link href=''>market</Link>
-            <Link href=''>dekhor</Link>
-            <Link href=''>hongkrua</Link>
+        <div style={{alignItems:"center",justifyContent:"space-around",width:'100%',height:'100vh',display:'flex'}}>
+            <Link style={{textDecoration:'none'}} href='https://linux-vm-southeastasia-2.southeastasia.cloudapp.azure.com/dorms'>
+                <div className={style.portal}>
+                    <div className={style.icon}><img className={style.bg_portal} src="image/dekhordorm_portal.png"/></div>
+                    <p className={style.subtitle_portal}>DekHor Dorms</p>
+                </div>
+            </Link>
+            <Link style={{textDecoration:'none'}} href='https://linux-vm-southeastasia-2.southeastasia.cloudapp.azure.com/market'>
+                <div className={style.portal}>
+                    <div className={style.icon}><img className={style.bg_portal} src="image/dekhormarket_portal.png"/></div>
+                    <p className={style.subtitle_portal}>DekHor Markets</p>
+                </div>
+            </Link>
+            <Link style={{textDecoration:'none'}} href='https://linux-vm-southeastasia-2.southeastasia.cloudapp.azure.com/blogs'>
+                <div className={style.portal}>
+                    <div className={style.icon}><img className={style.bg_portal} src="image/dekhorblog_portal.png"/></div>
+                    <p className={style.subtitle_portal}>DekHor Blogs</p>
+                </div>
+                </Link>
+            <Link style={{textDecoration:'none'}} href='https://linux-vm-southeastasia-2.southeastasia.cloudapp.azure.com/eats'>
+                <div className={style.portal}>
+                    <div className={style.icon}><img className={style.bg_portal} src="image/dekhoreat_portal.png"/></div>
+                    <p className={style.subtitle_portal}>DekHor Eats</p>
+                </div>
+            </Link>
+            
         </div>
     )
 }
