13a33b785b730a5371900e556edd16263fafa5e7
Thanabordee <s5901012630075@email.kmutnb.ac.th>
2024-02-04 16:37:31 +0700

    Test with selenium to open website

---
+++ Testcase/Test_selenium.js
@@ -0,0 +1,27 @@
+
+const { Builder, By, Key, until } = require('selenium-webdriver');
+const firefox = require('selenium-webdriver/firefox');
+
+// Set the path to the Firefox executable (you may skip this if Firefox is in your system's PATH)
+const firefoxOptions = new firefox.Options().setBinary('path/to/firefox');
+
+// Create a new instance of the Firefox driver
+const driver = new Builder()
+  .forBrowser('firefox')
+  .setFirefoxOptions(firefoxOptions)
+  .build();
+
+// Navigate to web
+(async function example() {
+  try {
+    await driver.get('https://linux-vm-southeastasia-2.southeastasia.cloudapp.azure.com/develop');
+
+    // Wait until the title of the page becomes "home"
+    await driver.wait(until.titleIs('home'), 1000);
+
+    console.log('Successfully opened Dekhor in Firefox.');
+  } finally {
+    // Close the browser
+    await driver.quit();
+  }
+})();
