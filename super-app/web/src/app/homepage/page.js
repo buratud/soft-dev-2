0d2bc9a3eb2e7682a3ad5593e5a72a927cc38277
Buratud Thumboon <buratud@gmail.com>
2024-02-04 15:06:55 +0700

    Fix react env for superapp

--- super-app/web/src/app/homepage/page.js
+++ super-app/web/src/app/homepage/page.js
@@ -1,7 +1,7 @@
 'use client'
 import Link from "next/link";
 import style from "./page.module.css";
-
+import { NEXT_PUBLIC_BASE_WEB_URL } from "../config";
 export default function Home() {
     return (
         
@@ -23,25 +23,25 @@ export default function Home() {
                 </div>
             </div>
             <div style={{marginTop:"150px",justifyContent:"space-around",width:'100%',height:'100vh',display:'flex'}}>
-                <Link style={{textDecoration:'none'}} href='https://linux-vm-southeastasia-2.southeastasia.cloudapp.azure.com/dorms'>
+                <Link style={{textDecoration:'none'}} href={`${NEXT_PUBLIC_BASE_WEB_URL}/dorms`}>
                     <div className={style.portal}>
                         <div className={style.icon}><img className={style.bg_portal} src="image/dekhordorm_portal.png"/></div>
                         <p className={style.subtitle_portal}>DekHor Dorms</p>
                     </div>
                 </Link>
-                <Link style={{textDecoration:'none'}} href='https://linux-vm-southeastasia-2.southeastasia.cloudapp.azure.com/market'>
+                <Link style={{textDecoration:'none'}} href={`${NEXT_PUBLIC_BASE_WEB_URL}/market`}>
                     <div className={style.portal}>
                         <div className={style.icon}><img className={style.bg_portal} src="image/dekhormarket_portal.png"/></div>
                         <p className={style.subtitle_portal}>DekHor Markets</p>
                     </div>
                 </Link>
-                <Link style={{textDecoration:'none'}} href='https://linux-vm-southeastasia-2.southeastasia.cloudapp.azure.com/blogs'>
+                <Link style={{textDecoration:'none'}} href={`${NEXT_PUBLIC_BASE_WEB_URL}/blogs`}>
                     <div className={style.portal}>
                         <div className={style.icon}><img className={style.bg_portal} src="image/dekhorblog_portal.png"/></div>
                         <p className={style.subtitle_portal}>DekHor Blogs</p>
                     </div>
                     </Link>
-                <Link style={{textDecoration:'none'}} href='https://linux-vm-southeastasia-2.southeastasia.cloudapp.azure.com/eats'>
+                <Link style={{textDecoration:'none'}} href={`${NEXT_PUBLIC_BASE_WEB_URL}/eats`}>
                     <div className={style.portal}>
                         <div className={style.icon}><img className={style.bg_portal} src="image/dekhoreat_portal.png"/></div>
                         <p className={style.subtitle_portal}>DekHor Eats</p>
