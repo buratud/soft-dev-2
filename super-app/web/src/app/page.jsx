789a7279b92d4c108c7b99a94cba28027e6ca6dd
Buratud Thumboon <buratud@gmail.com>
2024-02-10 22:51:19 +0700

    Revert "link edit home"
    This reverts commit ef451635ee1c676451a24241fd9615ce7e499e3b.

--- super-app/web/src/app/page.jsx
+++ super-app/web/src/app/page.jsx
@@ -89,19 +89,19 @@ export default function Home() {
       </div>
 
         <div className={styles.portalwrap}>
-          <Link style={{ textDecoration: 'none' }} href={`/dorms`}>
+          <Link style={{ textDecoration: 'none' }} href={`${NEXT_PUBLIC_BASE_WEB_PATH}/dorms`}>
             <div className={styles.portal}>
               <div className={styles.icon}><img className={styles.bg_portal} src={`${NEXT_PUBLIC_BASE_WEB_PATH}/image/dekhordorm_portal.png`} /></div>
               <p className={styles.subtitle_portal}>DekHor Dorms</p>
             </div>
           </Link>
-          <Link style={{ textDecoration: 'none' }} href={`/markets`}>
+          <Link style={{ textDecoration: 'none' }} href={`${NEXT_PUBLIC_BASE_WEB_PATH}/markets`}>
             <div className={styles.portal}>
               <div className={styles.icon}><img className={styles.bg_portal} src={`${NEXT_PUBLIC_BASE_WEB_PATH}/image/dekhormarket_portal.png`} /></div>
               <p className={styles.subtitle_portal}>DekHor Markets</p>
             </div>
           </Link>
-          <Link style={{ textDecoration: 'none' }} href={`/blogs`}>
+          <Link style={{ textDecoration: 'none' }} href={`${NEXT_PUBLIC_BASE_WEB_PATH}/blogs`}>
             <div className={styles.portal}>
               <div className={styles.icon}><img className={styles.bg_portal} src={`${NEXT_PUBLIC_BASE_WEB_PATH}/image/dekhorblog_portal.png`} /></div>
               <p className={styles.subtitle_portal}>DekHor Blogs</p>
@@ -123,7 +123,7 @@ export default function Home() {
               <div className={styles.poster_info_blog}>
                 <h1>Start Your Blog Today!</h1>
                 <p>Share tips and tricks from your DekHor experience!</p>
-                <Link href={`/blogs/writeblog`} style={{ textDecoration: 'none' }}>
+                <Link href={`${NEXT_PUBLIC_BASE_WEB_PATH}/blogs/writeblog`} style={{ textDecoration: 'none' }}>
                   <div className={styles.poster_button_blog}>
                     <p style={{ marginRight: '10px' }}>Blogging</p>
                     <img src={`${NEXT_PUBLIC_BASE_WEB_PATH}/image/arrow_right.png`} style={{ width: '29px', height: '21px' }} />
@@ -167,7 +167,7 @@ export default function Home() {
                 Verified Reviews <br />
                 Affordable Prices
               </p>
-              <Link href={`/dorms`} style={{ textDecoration: 'none' }}>
+              <Link href={`${NEXT_PUBLIC_BASE_WEB_PATH}/dorms`} style={{ textDecoration: 'none' }}>
                 <div className={styles.poster_button_discover}>
                   <p style={{ marginRight: '10px' }}>Discover More</p>
                   <img src={`${NEXT_PUBLIC_BASE_WEB_PATH}/image/arrow_right.png`} style={{ width: '29px', height: '21px' }} />
@@ -190,7 +190,7 @@ export default function Home() {
               <div className={styles.poster_info_market}>
                 <h1>Find the stuffs you need
                   <br />with Dekhor Markets!</h1>
-                <Link href={`/markets`} style={{ textDecoration: 'none' }}>
+                <Link href={`${NEXT_PUBLIC_BASE_WEB_PATH}/markets`} style={{ textDecoration: 'none' }}>
                   <div className={styles.poster_button_discover}>
                     <p style={{ marginRight: '10px' }}>Discover More</p>
                     <img src={`${NEXT_PUBLIC_BASE_WEB_PATH}/image/arrow_right.png`} style={{ width: '29px', height: '21px' }} />
