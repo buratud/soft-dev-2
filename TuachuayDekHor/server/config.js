94187b305664c746cccd2a336af54db2a6c12fef
Buratud Thumboon <buratud@gmail.com>
2024-02-11 20:00:00 +0700

    Fix config for blogs server

--- TuachuayDekHor/server/config.js
+++ TuachuayDekHor/server/config.js
@@ -1,7 +1,4 @@
-const BASE_DOMAIN_WITH_PROTOCOL = process.env.BASE_DOMAIN_WITH_PROTOCOL;
-const BASE_SERVER_PATH = process.env.BASE_SERVER_PATH || '/api/blogs';
-const BASE_WEB_PATH = process.env.BASE_WEB_PATH || '/blogs';
+exports.BASE_SERVER_PATH = process.env.BASE_SERVER_PATH || '/';
 exports.PORT = process.env.PORT || 5000;
 exports.SUPABASE_URL = process.env.SUPABASE_URL;
 exports.SUPABASE_KEY = process.env.SUPABASE_KEY;
-exports.BASE_SERVER_PATH = BASE_SERVER_PATH;
