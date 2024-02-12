565cec2cef30113617faae7e63b0e8e6f3b07126
Buratud Thumboon <buratud@gmail.com>
2024-02-12 13:46:03 +0700

    Abstract server routes

--- MarketConnect/server/src/main.ts
+++ MarketConnect/server/src/main.ts
@@ -3,6 +3,26 @@ import { BASE_SERVER_PATH, PORT } from './config';
 
 const app = express.Router();
 
+app.get('/best-sellers', (req, res) => {
+    return res.status(200).json({ "message": "Hello, world!" });
+});
+
+app.get('/new-arrivals', (req, res) => {
+    return res.status(200).json({ "message": "Hello, world!" });
+});
+
+app.get('/products', (req, res) => {
+    return res.status(200).json({ "message": "Hello, world!" });
+});
+
+app.get('/products/:id', (req, res) => {
+    return res.status(200).json({ "message": "Hello, world!" });
+});
+
+app.post('/products', (req, res) => {
+    return res.status(200).json({ "message": "Hello, world!" });
+})
+
 express().use(app).listen(PORT, () => {
     console.log(`Server is listening on port ${PORT} at ${BASE_SERVER_PATH}`);
 });
