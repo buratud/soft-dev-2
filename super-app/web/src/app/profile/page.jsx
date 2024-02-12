c32b3a0e013160699445b22f2c16cbf7d7fa4b7a
Korrakod Poonpradit <s6501012610017@email.kmutnb.ac.th>
2024-02-12 13:30:20 +0700

    profile_blogs

--- super-app/web/src/app/profile/page.jsx
+++ super-app/web/src/app/profile/page.jsx
@@ -6,38 +6,43 @@ import style from "./page.module.css"
 import Footer from "../../../components/footer/Footer"
 
 export default function Profile() {
-  return (
-    
-    <div className={style.profile_page}>
-        <NavBar></NavBar>
-        <div className={style.container}>
 
-            <div className={style.profile}>Profile</div>
-            
-            <div className={style.user}>
-                <img className={style.user_img} src="images/user_img.png"/>
-                <div className={style.user_info}>
-                    <div className={style.username}>Username</div>
-                    <Link href={''} className={style.edit_profile_button}>
-                        <div className={style.edit_profile_img}>
-                            <img src="images/edit_profile.png"/>
-                        </div>
-                        <div className={style.edit_profile}>Edit profile</div>
-                    </Link>
-                    <div className={style.line}/>
-                    <select className={style.dropdown}>
-                        <option value="volvo">DekHor Blogs</option>
-                        <option value="saab">DekHor Dorms</option>
-                        <option value="mercedes">DekHor Markets</option>
-                    </select>
+    
+    const BlogsProfile = () => {
+       
+    }
+    return (
+
+        <div className={style.profile_page}>
+            <NavBar></NavBar>
+            <div className={style.container}>
+
+                <div className={style.profile}>Profile</div>
+
+                <div className={style.user}>
+                    <img className={style.user_img} src="images/user_img.png" />
+                    <div className={style.user_info}>
+                        <div className={style.username}>Username</div>
+                        <Link href={''} className={style.edit_profile_button}>
+                            <div className={style.edit_profile_img}>
+                                <img src="images/edit_profile.png" />
+                            </div>
+                            <div className={style.edit_profile}>Edit profile</div>
+                        </Link>
+                        <div className={style.line} />
+                        <select className={style.dropdown}>
+                            <option value="volvo">DekHor Blogs</option>
+                            <option value="saab">DekHor Dorms</option>
+                            <option value="mercedes">DekHor Markets</option>
+                        </select>
+                    </div>
+                </div>
+
+                <div className={style.data}>
+
                 </div>
             </div>
-
-            <div className={style.data}>
-
-            </div>
+            <Footer></Footer>
         </div>
-        <Footer></Footer>
-    </div>
-  )
+    )
 }
