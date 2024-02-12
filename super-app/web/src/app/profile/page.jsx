eb0fa5d1437f4872cfe601928a7a65217409fdaa
Korrakod Poonpradit <s6501012610017@email.kmutnb.ac.th>
2024-02-13 00:03:17 +0700

    link img

--- super-app/web/src/app/profile/page.jsx
+++ super-app/web/src/app/profile/page.jsx
@@ -4,10 +4,12 @@ import Link from "next/link"
 import NavBar from "../../../components/nav"
 import style from "./page.module.css"
 import Footer from "../../../components/footer/Footer"
+import {NEXT_PUBLIC_BASE_WEB_PATH} from "../../../config.js";
 
 const BlogsProfile = () => {
        return(
-            <div>blog</div>
+            <div>
+            </div>
        )
     }
 
@@ -22,12 +24,12 @@ export default function Profile() {
                 <div className={style.profile}>Profile</div>
 
                 <div className={style.user}>
-                    <img className={style.user_img} src="images/user_img.png" />
+                    <img className={style.user_img} src={`${NEXT_PUBLIC_BASE_WEB_PATH}images/user_img.png`} />
                     <div className={style.user_info}>
                         <div className={style.username}>Username</div>
                         <Link href={''} className={style.edit_profile_button}>
                             <div className={style.edit_profile_img}>
-                                <img src="images/edit_profile.png" />
+                                <img src={`${NEXT_PUBLIC_BASE_WEB_PATH}images/edit_profile.png`} />
                             </div>
                             <div className={style.edit_profile}>Edit profile</div>
                         </Link>
