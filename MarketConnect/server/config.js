7e280760483dc8fc8ec2d73fbead421372e65504
NickKhunapoj <Nickkipad@gmail.com>
2024-02-12 10:34:20 +0700

    still make bugs

--- MarketConnect/server/config.js
+++ MarketConnect/server/config.js
@@ -1,4 +1,2 @@
-exports.BASE_SERVER_PATH = BASE_SERVER_PATH || '/';
-exports.PORT = process.env.PORT || 5000;
-exports.SUPABASE_URL = process.env.SUPABASE_URL;
-exports.SUPABASE_KEY = process.env.SUPABASE_KEY;
+module.exports.supabaseUrl = process.env.SUPABASE_URL;
+module.exports.supabaseKey = process.env.SUPABASE_KEY;
--- MarketConnect/server/server.js
+++ MarketConnect/server/server.js
@@ -3,31 +3,29 @@ const express = require("express");
 const cors = require("cors");
 const { decode } = require("base64-arraybuffer");
 const { createClient } = require("@supabase/supabase-js");
-const { SUPABASE_URL, SUPABASE_KEY, PORT, BASE_SERVER_PATH } = require("./config");
+const { supabaseKey, supabaseUrl } = require("./config");
 const { search } = require("./search");
 
-const supabase = createClient(SUPABASE_URL, SUPABASE_KEY);
+const supabase = createClient(supabaseUrl, supabaseKey);
 
 const app = express();
-const api = express.Router();
-
-app.use(BASE_SERVER_PATH, api);
-
 app.use(cors());
 app.use(express.json({ limit: "50mb" }));
 
-api.get('/testget', (req, res) => {
+const port = process.env.PORT || 5200;
+
+app.get('/testget', (req, res) => {
   res.status(200).json({ message: 'Hello from server!' });
 });
 
-api.post("/testpost", (req, res) => {
+app.post("/testpost", (req, res) => {
   const { email, password } = req.body;
   const data = { email: email, password: password };
   console.log(data);
   res.status(200).json(data);
 });
 
-api.post("/search", async (req, res) => {
+app.post("/search", async (req, res) => {
   const { searchTerm } = req.body;
   const { data, error } = await supabase.from("MarketConnect_Food").select("id, Food_Name, Price, URL");
   const result = search(searchTerm, data);
@@ -40,7 +38,7 @@ api.post("/search", async (req, res) => {
   }
 });
 
-api.post("/register", async (req, res) => {
+app.post("/register", async (req, res) => {
   const { email, password } = req.body;
   const { data, error } = await supabase.auth.signUp({
     email: email,
@@ -54,7 +52,7 @@ api.post("/register", async (req, res) => {
   }
 });
 
-api.post("/sendsupport", async (req, res) => {
+app.post("/sendsupport", async (req, res) => {
   const { email, message, status, contact } = req.body;
   const { data, error } = await supabase.from("MarketConnect_Support").insert({
     Sender: email,
@@ -69,7 +67,7 @@ api.post("/sendsupport", async (req, res) => {
   }
 });
 
-api.post("/getsupport", async (req, res) => {
+app.post("/getsupport", async (req, res) => {
   const { email } = req.body;
   const { data, error } = await supabase
     .from("MarketConnect_Support")
@@ -82,7 +80,7 @@ api.post("/getsupport", async (req, res) => {
   }
 });
 
-api.post("/adminsupport", async (req, res) => {
+app.post("/adminsupport", async (req, res) => {
   const { data, error } = await supabase
     .from("MarketConnect_Support")
     .select("Problem,Status,id,Sender,Contact");
@@ -93,7 +91,7 @@ api.post("/adminsupport", async (req, res) => {
   }
 });
 
-api.post("/changestatus", async (req, res) => {
+app.post("/changestatus", async (req, res) => {
   const { status, id } = req.body;
   const { data, error } = await supabase
     .from("MarketConnect_Support")
@@ -106,7 +104,7 @@ api.post("/changestatus", async (req, res) => {
   }
 });
 
-api.post("/unsendsupport", async (req, res) => {
+app.post("/unsendsupport", async (req, res) => {
   const { id } = req.body;
   const { data, error } = await supabase
     .from("MarketConnect_Support")
@@ -119,12 +117,12 @@ api.post("/unsendsupport", async (req, res) => {
   }
 });
 
-api.post("/fooddetail", async (req, res) => {
+app.post("/fooddetail", async (req, res) => {
   const { foodid } = req.body;
   const { data, error } = await supabase
     .from("MarketConnect_Food")
     .select(
-      "Food_Name, Price, Description, URL,Line,Catagory_Id, users(firstname,lastname,contact), MarketConnect_Category(catagory_name)"
+      "Food_Name, Price, Description, URL,Line,Catagory_Id"
     )
     .eq("id", foodid);
   if (error) {
@@ -134,7 +132,7 @@ api.post("/fooddetail", async (req, res) => {
   }
 });
 
-api.post("/food", async (req, res) => {
+app.post("/food", async (req, res) => {
   const { data, error } = await supabase
     .from("MarketConnect_Food")
     .select("id, Food_Name, Price,URL");
@@ -145,7 +143,7 @@ api.post("/food", async (req, res) => {
   }
 });
 
-api.post("/getAdmin", async (req, res) => {
+app.post("/getAdmin", async (req, res) => {
   const { user } = req.body;
   const { data, error } = await supabase
     .from("User")
@@ -159,7 +157,7 @@ api.post("/getAdmin", async (req, res) => {
   }
 });
 
-api.post("/yourfood", async (req, res) => {
+app.post("/yourfood", async (req, res) => {
   const { user } = req.body;
   const { data, error } = await supabase
     .from("MarketConnect_Food")
@@ -172,7 +170,7 @@ api.post("/yourfood", async (req, res) => {
   }
 });
 
-api.post("/new", async (req, res) => {
+app.post("/new", async (req, res) => {
   const { data, error } = await supabase
     .from("MarketConnect_Food")
     .select("id, Food_Name, Price, URL")
@@ -185,8 +183,8 @@ api.post("/new", async (req, res) => {
   }
 });
 
-api.post("/pro", async (req, res) => {
-  const { data, error } = await supabase.from("Promotion").select(" URL");
+app.post("/pro", async (req, res) => {
+  const { data, error } = await supabase.from("MarketConnect_Promotion").select(" URL");
   if (error) {
     res.status(400).json(error);
   } else {
@@ -194,7 +192,7 @@ api.post("/pro", async (req, res) => {
   }
 });
 
-api.post("/verify", async (req, res) => {
+app.post("/verify", async (req, res) => {
   const { email, token } = req.body;
   const { data, error } = await supabase.auth.verifyOtp({
     email,
@@ -208,7 +206,7 @@ api.post("/verify", async (req, res) => {
   }
 });
 
-api.post("/save", async (req, res) => {
+app.post("/save", async (req, res) => {
   const img = decode(req.body.img);
   const { firstname, lastname, contact, id } = req.body;
   const { data, error } = await supabase.auth.admin.updateUserById(id, {
@@ -239,7 +237,7 @@ api.post("/save", async (req, res) => {
   }
 });
 
-api.post("/delete", async (req, res) => {
+app.post("/delete", async (req, res) => {
   const { food } = req.body;
   const { data, error } = await supabase
     .from("MarketConnect_Food")
@@ -252,7 +250,7 @@ api.post("/delete", async (req, res) => {
   }
 });
 
-api.post("/addproduct", async (req, res) => {
+app.post("/addproduct", async (req, res) => {
   const { name, price, catagory_id, id, description, picture, line } = req.body;
   const { data, error } = await supabase.from("MarketConnect_Food").insert({
     Food_Name: name,
@@ -275,7 +273,7 @@ api.post("/addproduct", async (req, res) => {
   }
 });
 
-api.post("/manageproduct", async (req, res) => {
+app.post("/manageproduct", async (req, res) => {
   const {
     food,
     name,
@@ -305,4 +303,4 @@ api.post("/manageproduct", async (req, res) => {
   }
 });
 
-app.listen(PORT, () => console.log(`Express app running on port ${PORT}`));
+app.listen(port, () => console.log(`Express app running on port ${port}`));
