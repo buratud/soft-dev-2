f0f9d99cfb9c6e571d762cdad17303b665a62bf6
Buratud Thumboon <buratud@gmail.com>
2024-02-12 12:48:17 +0700

    Delete everything

--- MarketConnect/server/config.js
+++
@@ -1,4 +0,0 @@
-exports.BASE_SERVER_PATH = process.env.BASE_SERVER_PATH || '/';
-exports.PORT = process.env.PORT || 5000;
-exports.SUPABASE_URL = process.env.SUPABASE_URL;
-exports.SUPABASE_KEY = process.env.SUPABASE_KEY;
