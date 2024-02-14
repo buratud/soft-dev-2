b3942cb0a97b794e790d14761e6feb8506148b4d
Buratud Thumboon <buratud@gmail.com>
2024-02-14 20:26:25 +0700

    Update port superapp server to 4000

--- super-app/server/config.js
+++ super-app/server/config.js
@@ -1,4 +1,4 @@
-exports.PORT = process.env.PORT || 5000;
+exports.PORT = process.env.PORT || 4000;
 exports.BASE_SERVER_PATH = process.env.BASE_SERVER_PATH || '/';
 exports.SUPABASE_URL = process.env.SUPABASE_URL;
 exports.SUPABASE_KEY = process.env.SUPABASE_KEY;
