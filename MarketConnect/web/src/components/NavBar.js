48f6c342aea7e2b81c9d997d148470e3be9b0e85
Buratud Thumboon <buratud@gmail.com>
2024-02-15 15:04:19 +0700

    Broken navigation bar (push anyway)

--- MarketConnect/web/src/components/NavBar.js
+++ MarketConnect/web/src/components/NavBar.js
@@ -1,6 +1,7 @@
 import { NavLink } from "react-router-dom";
-import "./NavBar.scoped.css";
+import "./NavBar.scoped.sass";
 import { useSupabase } from "../App";
+import { REACT_APP_BASE_WEB_PATH } from "../config";
 
 const NavBar = () => {
   const supabase = useSupabase();
@@ -10,31 +11,70 @@ const NavBar = () => {
     supabase.auth.signOut();
   };
   return (
-    <header>
-      <NavLink className="logo" to="/home">
-        <img src="/logonav.svg" alt="" />
-      </NavLink>
-      <nav className="nav-bar">
-        <NavLink className={nav_class} to="/home">
-          Home <NavIcon img="/homeicon.png" />
-        </NavLink>
-        <NavLink className={nav_class} to="/food">
-          Food <NavIcon img="/foodicon.png" />
-        </NavLink>
-        <NavLink className={nav_class} to="/manage">
-          Manage <NavIcon img="/manage.png" />
-        </NavLink>
-        <NavLink className={nav_class} to="/support">
-          Support <NavIcon img="/supporticon.png" />
-        </NavLink>
-        <NavLink className={nav_class} to="/profile">
-          Profile <NavIcon img="/profileicon.png" />
-        </NavLink>
-        <NavLink className="nav_copy" onClick={Sign_Out}>
-          Logout <NavIcon img="/logouticon.png" />
-        </NavLink>
-      </nav>
-    </header>
+    <nav>
+      <div>
+        <img src={`${REACT_APP_BASE_WEB_PATH}/logo.png`} className="logo" />
+      </div>
+      <ul className="menu">
+        <li>
+          <span>Blogs</span>
+          <ul className="main hidden">
+            <li>Main</li>
+            <li className="sub-menu">
+              <span>Categories &gt;</span>
+              <ul className="hidden left">
+                <li>Cleaning</li>
+                <li>Decorations</li>
+                <li>Cooking</li>
+                <li>DekHor's Story</li>
+              </ul>
+            </li>
+            <li>Blogging</li>
+            <li>Blogger</li>
+          </ul>
+        </li>
+        <li>
+          <span>Dorms</span>
+          <ul className="main hidden">
+            <li>Main</li>
+            <li>All Dorms</li>
+            <li>Add Dorm</li>
+          </ul>
+        </li>
+        <li>
+          <span>Markets</span>
+          <ul className="main hidden">
+            <li>Main</li>
+            <li>All Products</li>
+            <li>Manage Products</li>
+          </ul>
+        </li>
+      </ul>
+      <div className="right">
+        <>
+          <a className="button outlined">Sign up</a>
+          <div className="divider" />
+          <a className="button primary">Login</a>
+        </>
+        {/* <div>
+          <img src={`${REACT_APP_BASE_WEB_PATH}/logo.png`} className="logo" />
+          <ul>
+            <li>
+              <img src={`${REACT_APP_BASE_WEB_PATH}/logo.png`} className="logo" />
+              <span>My Profile</span>
+            </li>
+            <li>
+              <img src={`${REACT_APP_BASE_WEB_PATH}/logo.png`} className="logo" />
+              <span>Support</span>
+            </li>
+            <li>
+              <img src={`${REACT_APP_BASE_WEB_PATH}/logo.png`} className="logo" />
+              <span>Log out</span>
+            </li>
+          </ul>
+        <div/> */}
+      </div>
+    </nav>
   );
 };
 
