d6a54f8298d5bda67100f8eb400137cb5c0557cc
SleepyO <141929050+SleepyOo0@users.noreply.github.com>
2024-02-17 16:00:28 +0700

    Update Footer.jsx

--- super-app/web/components/footer/Footer.jsx
+++ super-app/web/components/footer/Footer.jsx
@@ -7,23 +7,22 @@ import { NEXT_PUBLIC_BASE_WEB_PATH } from '../../config'
 export default function Footer() {
     return (
         <div className={styles.footerframe}>
-            <Image src={`${NEXT_PUBLIC_BASE_WEB_PATH}/DekhorlogoTrans.png`} width={96} height={96} alt="logo"/>
+            <img src={`${NEXT_PUBLIC_BASE_WEB_PATH}/DekhorlogoTrans.png`} className={styles.logo} alt="logo"/>
             <div className={styles.footercontent}> 
                 <div className={styles.footernavbox}>
-                    <Link className={styles.footernav} href="/homepage">Home</Link>
+                    <Link className={styles.footernav} href="/">Home</Link>
                     <Link className={styles.footernav} href="/blogs">Blogs</Link>
                     <Link className={styles.footernav} href="/dorms">Dorms</Link>
-                    <Link className={styles.footernav} href="/eats">Eats</Link>
-                    <Link className={styles.footernav} href="/market">Markets</Link>
+                    <Link className={styles.footernav} href="/markets">Markets</Link>
                 </div>
                 <div className={styles.footercontactbox}>
                     <b>Contact Us</b>
                     <div className={styles.footercontact}> 
-                        <Image src ={`${NEXT_PUBLIC_BASE_WEB_PATH}/EmailFtico.png`} alt = "email" width={20} height={20}/> Email : contact@dekhorsuperapp.com </div>
+                        <Image src ={`${NEXT_PUBLIC_BASE_WEB_PATH}/EmailFtico.png`} alt = "email" width={20} height={20} className={styles.icon}/> Email : contact@dekhorsuperapp.com </div>
                     <div className={styles.footercontact}>
-                        <Image src ={`${NEXT_PUBLIC_BASE_WEB_PATH}/FBftico.png`} alt = "FB" width={20} height={20}/> Facebook : DekHor Super App </div>
+                        <Image src ={`${NEXT_PUBLIC_BASE_WEB_PATH}/FBftico.png`} width={20} height={20} className={styles.icon} alt = "FB" /> Facebook : DekHor Super App </div>
                     <div className={styles.footercontact}>
-                        <Image src ={`${NEXT_PUBLIC_BASE_WEB_PATH}/phonecallico.png`} alt = "phone" width={20} height={20}/> 02-XXX-XXXX </div>
+                        <Image src ={`${NEXT_PUBLIC_BASE_WEB_PATH}/phonecallico.png`} width={20} height={20} className={styles.icon} alt = "phone" /> 02-XXX-XXXX </div>
                 </div>
             </div>
             <div className={styles.footerline}></div>
