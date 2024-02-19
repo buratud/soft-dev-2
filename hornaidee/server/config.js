77489eafaed1268762c9b569f97a65e3046d8b49
Buratud Thumboon <buratud@gmail.com>
2024-02-19 23:29:25 +0700

    Reset dorms server

--- hornaidee/server/config.js
+++ hornaidee/server/config.js
@@ -1,13 +1,4 @@
-const BASE_DOMAIN_WITH_PROTOCOL = process.env.BASE_DOMAIN_WITH_PROTOCOL;
-const BASE_SERVER_PATH = process.env.BASE_SERVER_PATH || '/';
-const BASE_WEB_PATH = process.env.BASE_WEB_PATH || '/';
-exports.BASE_WEB_PATH = BASE_WEB_PATH;
-exports.BASE_SERVER_PATH = BASE_SERVER_PATH;
-exports.BASE_API_URL = `${BASE_DOMAIN_WITH_PROTOCOL}${BASE_SERVER_PATH}`;
 exports.PORT = process.env.PORT || 4003;
-exports.JWT_KEY = process.env.JWT_KEY;
-exports.MYSQL_HOST = process.env.MYSQL_HOST;
-exports.MYSQL_USER = process.env.MYSQL_USER;
-exports.MYSQL_PASSWORD = process.env.MYSQL_PASSWORD;
-exports.MYSQL_DATABASE = process.env.MYSQL_DATABASE;
-exports.WEB_URL = `${BASE_DOMAIN_WITH_PROTOCOL}${BASE_WEB_PATH}` || 'http://localhost:3003';
+exports.BASE_SERVER_PATH = process.env.BASE_SERVER_PATH || '/';
+exports.SUPABASE_URL = process.env.SUPABASE_URL;
+exports.SUPABASE_KEY = process.env.SUPABASE_KEY;
