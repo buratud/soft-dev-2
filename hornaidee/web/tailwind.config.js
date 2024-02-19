bd2249c738c445ff1b9754550c8e33959f883e90
Phutawan Chanrueng <phutawanchanrueng@gmail.com>
2024-02-19 13:30:35 +0700

    Create new Next.js project

--- hornaidee/web/tailwind.config.js
+++ hornaidee/web/tailwind.config.js
@@ -1,18 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{js,jsx,ts,tsx}",],
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
   theme: {
     extend: {
-      colors: {
-        "brown":"rgba(160, 133, 91, 1)",
-        "old_green":"rgba(56, 71, 11, 1)",
-        "cream": "rgba(249, 246, 242, 1)",
-        "light_gray" :"rgba(245, 245, 245, 1)",
-        "gray" : "rgba(210, 210, 210, 1)",
-        "old_yellow" :"rgba(241, 214, 171, 1)"
-      }
+      backgroundImage: {
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      },
     },
   },
   plugins: [],
-}
-
+};
