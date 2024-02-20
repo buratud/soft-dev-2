a59564765c404e4c3bf1b9ac20c5a41664f5952e
s6501012610033 <s6501012610033@kmutnb.ac.th>
2024-02-20 19:43:48 +0700

    add api for blogger page

--- hornaidee/server/server.js
+++ hornaidee/server/server.js
@@ -6,4 +6,38 @@ app.get("/debug-sentry", function mainHandler(req, res) {
     throw new Error("My second Sentry error!");
 });
 
+//-----------------------------Blogger-----------------------------------
+
+app.post('/blogger_list', async (req, res) => {
+    try {
+      const { data, error } = await supabase
+        .from('blogger')
+        .select('*')
+      if (error) {
+        throw error;
+      } else {
+        res.status(200).json(data);
+      }
+    } catch (error) {
+      res.status(500).json({ error: 'Internal Server Error' });
+    }
+  })
+  
+  // api.post('/search_blogger', async (req, res) => {
+  //   try {
+  //     const { data, error } = await supabase
+  //       .from('blog.blog')
+  //       .select('users(id)')
+  //     if (error) {
+  //       console.log(error)
+  //       throw error;
+  //     } else {
+  //       res.status(200).json(data);
+  //     }
+  //   } catch (err) {
+  //     console.log(err)
+  //     res.status(500).json({ error: 'Internal Server Error' });
+  //   }
+  // })
+
 module.exports = app;
