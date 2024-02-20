5406e40a0c112daff2178fea4044de9e169e227e
Buratud Thumboon <buratud@gmail.com>
2024-02-20 10:36:06 +0700

    Add `JWT_SECRET` environment variable

--- hornaidee/server/config.js
+++ hornaidee/server/config.js
@@ -2,3 +2,4 @@ exports.PORT = process.env.PORT || 4003;
 exports.BASE_SERVER_PATH = process.env.BASE_SERVER_PATH || '/';
 exports.SUPABASE_URL = process.env.SUPABASE_URL;
 exports.SUPABASE_KEY = process.env.SUPABASE_KEY;
+exports.JWT_SECRET = process.env.JWT_SECRET;
