49c03580cf2237b74c629a240e2e5d26c490c4e4
Korrakod Poonpradit <s6501012610017@email.kmutnb.ac.th>
2024-02-21 18:28:35 +0700

    add Search

--- TuachuayDekHor/web/src/component/Card/Card.jsx
+++ TuachuayDekHor/web/src/component/Card/Card.jsx
@@ -6,22 +6,20 @@ import { Link } from 'react-router-dom'
 const Card = (props) => {
     const { img, title, Blogger, Categories, id } = props;
     return (
-        <Link to={`/${Categories}/${id}`}>
-            <div className="singleDest">
-                <div className="dastImage">
-                    <img src={img} alt="" />
-                </div>
-                <div className="destFooter">
-                    <div className="destText">
-                        <h4>
-                            {title}
-                        </h4>
-                        <span className="userwrite">
-                            <span className="name"><BiSolidPencil size={20} className="icon_pencil" />
-                                {Blogger}
-                            </span>
+        <Link to={`/${Categories}/${id}`} className="singleDest">
+            <div className="dastImage">
+                <img src={img} alt="" />
+            </div>
+            <div className="destFooter">
+                <div className="destText">
+                    <p>
+                        {title}
+                    </p>
+                    <span className="userwrite">
+                        <span className="name"><BiSolidPencil size={20} className="icon_pencil" />
+                            {Blogger}
                         </span>
-                    </div>
+                    </span>
                 </div>
             </div>
         </Link>
--- TuachuayDekHor/web/src/pages/Search.jsx
+++ TuachuayDekHor/web/src/pages/Search.jsx
@@ -2,8 +2,7 @@ import React, { useState, useEffect } from 'react'
 import "./Search.scoped.css"
 import Navbar from '../component/Nav'
 import img1 from '../../src/Assets/slide1.png'
-import { BiSolidPencil } from "react-icons/bi";
-import { useLocation, Link } from 'react-router-dom'
+import { useLocation, } from 'react-router-dom'
 import axios from 'axios';
 import Footer from "../component/footer";
 import { Container } from 'reactstrap';
@@ -12,91 +11,64 @@ import Card from '../component/Card/Card';
 
 function Search() {
     const [data, setData] = useState([]);
+    const [searchQuery, setSearchQuery] = useState("");
+    const [searchResults, setSearchResults] = useState([]);
     // const { supabase_for_use: supabase, session, user } = useContext(General);
     useEffect(() => {
         axios.post(`${REACT_APP_BASE_API_URL}/search`, {
-
+            query: searchQuery
         })
             .then(res => {
-                console.log(res.data)
                 setData(res.data);
             })
-            .catch((err) => {
-                alert(err)
-            })
-    }, [])
-    const location = useLocation();
-    // const searchQuery = new URLSearchParams(location.search).get('query');
+            .catch(err => {
+                alert(err);
+            });
+    }, [searchQuery]);
 
-    // ใช้ค่า searchQuery ที่ได้รับจาก URL query parameter เป็นเงื่อนไขในการกรองข้อมูล Data 
-    // กรองข้อมูลที่ตรงกับคำค้นหา
-    // const filteredData = data.filter((item) => item.title.toLowerCase().includes(searchQuery.toLowerCase()));
+    const handleSearch = () => {
+        setSearchQuery(searchQuery);
+    };
+
+    const location = useLocation();
 
     return (
         <div className="search">
-            <header>
-                <Navbar />
-            </header>
-            <Container>
-                <div className="search_wrapper">
-                    <div className='headResult'>
-                        <h2>
-                            Search Results for :
-                            {/* <p>{searchQuery}</p> */}
-                        </h2>
-                    </div>
-                    <div className="showResult">
-                        <div className="main_content">
-                            {data.map((card, index) => (
-                                <Card
-                                    key={index}
-                                    img={card.image_link ?? card.img1}
-                                    title={card.title}
-                                    Blogger={card.user.username}
-                                    Categories={card.category}
-                                    id={card.id_post}
-                                />
-                            ))}
-                        </div>
-                    </div>
+            <Navbar/>
+            <div className="mainSearch">
+                <div className='input_Search'>
+                    <h2>
+                        Search Results for: {searchQuery}
+                    </h2>
+                    <input
+                        type="text"
+                        value={searchQuery}
+                        onChange={(e) => setSearchQuery(e.target.value)}
+                        placeholder="Enter search query..."
+                    />
+                    <button onClick={handleSearch}>Search</button>
                 </div>
-            </Container>
+                <div className="grid-container">
+                    {data.map((card, index) => (
+
+                        <Card
+                            key={index}
+                            img={card.image_link ?? img1}
+                            title={card.title}
+                            Blogger={card.user.username}
+                            Categories={card.category}
+                            id={card.id_post}
+                        />
+
+                    ))}
+                </div>
+            </div>
             <Footer></Footer>
         </div>
     )
-
 
 }
 
 export default Search
 
-{/* data.map(({ id_post: id, title, user: { username }, category, image_link }, index) => { */ }
-{/* //     return (
-//         <div className='grid-container'>
-//             <div className="singleDest" key={index}>
-//                 <div className="dastImage">
-//                     <img src={image_link ?? img1} alt="" />
-//                 </div>
-//                 <div className="destFooter">
-//                     <div className="destText">
-//                         <h4>
-//                             <Link to={`/${category}/${id}`}>{title}</Link>
-//                         </h4>
-//                         <span className="userwrite">
-//                             <span className="name"><BiSolidPencil size={20} className="icon_pencil" />
-//                                 {username}
-//                             </span>
-//                         </span>
-//                     </div>
-//                 </div>
-//             </div>
-//         </div>
-//     )
-// })
 
-// {data.length === 0 && (
-//     // ถ้าไม่พบผลการค้นหา
-//     <div className="noResults">
-//         <p>Sorry, no information found, please try another search.</p>
-//     </div>
-// )} */}
