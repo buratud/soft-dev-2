192f872a55f8e52109863b86342ad058726f82a0
Korrakod Poonpradit <s6501012610017@email.kmutnb.ac.th>
2024-02-22 00:43:38 +0700

    edit main blogger

--- TuachuayDekHor/web/src/pages/Blogger.jsx
+++ TuachuayDekHor/web/src/pages/Blogger.jsx
@@ -1,4 +1,4 @@
-import React,{useEffect,useState,useContext} from 'react'
+import React, { useEffect, useState, useContext } from 'react'
 import "./Blogger.scoped.css"
 import Navbar from '../component/Nav'
 import img1 from '../../src/Assets/person-circle-outline.svg'
@@ -11,35 +11,32 @@ function Blogger() {
   const [data, setData] = useState([]);
   const { supabase_for_use: supabase, session, user } = useContext(AuthContext);
   useEffect(() => {
-      axios.post(`${REACT_APP_BASE_API_URL}/blogger`,{
+    axios.post(`${REACT_APP_BASE_API_URL}/blogger`, {
 
-      })
+    })
       .then(res => {
         console.log(res.data)
         setData(res.data);
       })
       .catch((err) => {
-          alert(err)
+        alert(err)
       })
   }, [])
   return (
     <div className="Blogger">
-      <header>
-        <Navbar></Navbar>
-      </header>
+      <Navbar />
       <div className="main">
         <h1 className='title'>
           Our Blogger
         </h1>
-        {/* <p>Something</p> */}
         <div className="blogger_wrapper">
-        {
-            data.map(({user: { username, id }, image: {avatar_url}},index) => {
+          {
+            data.map(({ user: { username, id }, image: { avatar_url } }, index) => {
               return (
                 <div className="box">
                   <Link to={`/profile/${id}`}>
                     <div className="singleDest" key={index}>
-                      <img src={avatar_url??img1} alt=""/>
+                      <img src={avatar_url ?? img1} alt="" />
                     </div>
                     <div className="userwrite">
                       <Link to={`/profile/${id}`}>{username}</Link>
