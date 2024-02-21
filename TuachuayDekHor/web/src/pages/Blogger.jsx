cb8627e2686f6384492b5898cf76e22a95c18f86
Korrakod Poonpradit <s6501012610017@email.kmutnb.ac.th>
2024-02-22 02:44:09 +0700

    Search ค้นหาได้แล้ว(ยังไม่มี Search css)

--- TuachuayDekHor/web/src/pages/Blogger.jsx
+++ TuachuayDekHor/web/src/pages/Blogger.jsx
@@ -1,46 +1,64 @@
-import React, { useEffect, useState, useContext } from 'react'
-import "./Blogger.scoped.css"
-import Navbar from '../component/Nav'
-import img1 from '../../src/Assets/person-circle-outline.svg'
-import { Link } from 'react-router-dom'
+import React, { useEffect, useState, useContext } from 'react';
+import "./Blogger.scoped.css";
+import Navbar from '../component/Nav';
+import img1 from '../../src/Assets/person-circle-outline.svg';
+import { Link } from 'react-router-dom';
 import axios from 'axios';
 import { AuthContext } from '../App';
-import { REACT_APP_BASE_API_URL } from '../config'
+import { REACT_APP_BASE_API_URL } from '../config';
 import { IoSearch } from "react-icons/io5";
 
 function Blogger() {
   const [data, setData] = useState([]);
+  const [filteredData, setFilteredData] = useState([]);
+  const [searchInput, setSearchInput] = useState("");
   const { supabase_for_use: supabase, session, user } = useContext(AuthContext);
 
   useEffect(() => {
-    axios.post(`${REACT_APP_BASE_API_URL}/blogger`, {
-
-    })
+    axios.post(`${REACT_APP_BASE_API_URL}/blogger`)
       .then(res => {
-        console.log(res.data)
+        console.log(res.data);
         setData(res.data);
       })
       .catch((err) => {
-        alert(err)
-      })
-  }, [])
+        alert(err);
+      });
+  }, []);
 
+  useEffect(() => {
+    const filtered = data.filter(({ user: { username } }) =>
+      username.toLowerCase().includes(searchInput.toLowerCase())
+    );
+    setFilteredData(filtered);
+  }, [data, searchInput]);
+
+  const handleSearchInputChange = (event) => {
+    setSearchInput(event.target.value);
+  };
+
+  const handleSearchSubmit = (event) => {
+    event.preventDefault();
+    const filtered = data.filter(({ user: { username } }) =>
+      username.toLowerCase().includes(searchInput.toLowerCase())
+    );
+    setFilteredData(filtered);
+  };
 
   return (
     <div className="Blogger">
       <Navbar />
       <div className="main">
-        <h1 className='title'>
-          Our Blogger
-        </h1>
+        <h1 className='title'>Our Blogger</h1>
         <div className='Search_Wrapper'>
-          <form className='SearchBox'>
+          <form className='SearchBox' onSubmit={handleSearchSubmit}>
             <div className='Search_inside'>
               <IoSearch size={25} className="icon_Search" />
               <input
                 type="text"
                 placeholder="Search here..."
                 className='inputSearch'
+                value={searchInput}
+                onChange={handleSearchInputChange}
               />
             </div>
             <button type='submit' className='SearchBox_btn'>Search</button>
@@ -48,27 +66,23 @@ function Blogger() {
         </div>
         <div className="blogger_list">
           <div className="blogger_wrapper">
-            {
-              data.map(({ user: { username, id }, image: { avatar_url } }, index) => {
-                return (
-                  <div className="box">
-                    <Link to={`/profile/${id}`} className="Blog1">
-                      <div className="singleDest" key={index}>
-                        <img src={avatar_url ?? img1} alt="" />
-                      </div>
-                      <div className="userwrite">
-                        {username}
-                      </div>
-                    </Link>
+            {filteredData.map(({ user: { username, id }, image: { avatar_url } }, index) => (
+              <div className="box" key={index}>
+                <Link to={`/profile/${id}`} className="Blog1">
+                  <div className="singleDest">
+                    <img src={avatar_url ?? img1} alt="" />
                   </div>
-                )
-              })
-            }
+                  <div className="userwrite">
+                    {username}
+                  </div>
+                </Link>
+              </div>
+            ))}
           </div>
         </div>
       </div>
     </div>
-  )
+  );
 }
 
-export default Blogger
+export default Blogger;
