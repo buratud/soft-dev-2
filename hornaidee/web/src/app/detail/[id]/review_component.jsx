9124bf4476d06646bde1a956fb972c0ff8ff9e72
Phutawan Chanrueng <phutawanchanrueng@gmail.com>
2024-02-24 17:15:33 +0700

    Fetch username and avatar
    And allow new host in Next config

--- hornaidee/web/src/app/detail/[id]/review_component.jsx
+++ hornaidee/web/src/app/detail/[id]/review_component.jsx
@@ -1,20 +1,56 @@
 // Import useState and useEffect
 import { useState, useEffect } from "react";
 import { BsStarFill } from "react-icons/bs";
+import axios from "axios";
+import Image from "next/image";
+import { NEXT_PUBLIC_BASE_API_URL } from "../../../../config";
 
 export default function ReviewComponent({ user_id, stars, short_review, review }) {
+  const [user, setUser] = useState({});
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    // Fetch user data
+    axios.get(`${NEXT_PUBLIC_BASE_API_URL}/users/${user_id}`)
+      .then((res) => {
+        console.log("user data: ", res.data);
+        setUser(res.data);
+        setIsLoading(false);
+      });
+  }, [user_id]);
+
+  const handleImageError = () => {
+    // Handle the error when the image fetch fails (404 error)
+    console.log("Image fetch failed, using default image instead.");
+    setIsLoading(false); // Set loading to false to render the default image
+  };
+
   return (
     <main className="max-w-60 my-10 flex">
       {/* Picture profile */}
       <div className="flex items-center self-start break-words">
         <div className="rounded-full overflow-hidden w-20 h-20">
-          <img
-            src="https://www.w3schools.com/howto/img_avatar.png"
-            alt="Avatar"
-            className="w-full h-full object-cover"
-          />
+          {!isLoading ? (
+            <Image
+              src={user.picture}
+              alt="Avatar"
+              className="w-full h-full object-cover"
+              width={80}
+              height={80}
+              onError={handleImageError} // Handle image fetch error
+            />
+          ) : (
+            // Render a default image from the public folder if cannot fetch the image
+            <Image
+              src="/images/PersonCircle.svg"
+              alt="Default Avatar"
+              className="w-full h-full object-cover"
+              width={80}
+              height={80}
+            />
+          )}
         </div>
-        <div className="font-bold px-4 w-60">{user_id}</div>
+        <div className="font-bold px-4 w-60">{user.username}</div>
       </div>
 
       {/* Review */}
