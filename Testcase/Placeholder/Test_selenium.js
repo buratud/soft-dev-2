ddc8c1358a2b8421b76e1e9a335613de545c39da
Thanabordee <s5901012630075@email.kmutnb.ac.th>
2024-02-25 23:51:48 +0700

    Edit file

--- Testcase/Placeholder/Test_selenium.js
+++ Testcase/Placeholder/Test_selenium.js
@@ -1,6 +1,7 @@
-// This implement base on figma maynot operate with actual website.
 const { Builder, By, Key, until } = require('selenium-webdriver');
 const firefox = require('selenium-webdriver/firefox');
+const delay = ms => new Promise(res => setTimeout(res, ms));
+var assert = require('assert');
 
 // Set the path to the Firefox executable 
 const firefoxOptions = new firefox.Options().setBinary('path/to/firefox');
@@ -17,9 +18,17 @@ const driver = new Builder()
     await driver.get('https://linux-vm-southeastasia-2.southeastasia.cloudapp.azure.com/develop');
 
     // Wait until the title of the page becomes "home"
-    await driver.wait(until.titleIs('home'), 1000);
+    await driver.wait(until.titleIs('Create Next App'), 3000);
+
+    let attributeValue = await driver.findElement(By.className('nav_leftside__KJoZ9')).findElement(By.tagName('a')).getAttribute('href');
+    //.getAttribute('href');
+    //let attributeValue = await driver.findElement(By.className('nav_btn__oq_Li')).getAttribute('href');
+    console.log(attributeValue);
+    let attributeSignup = await driver.findElement(By.className('nav_btn__oq_Li')).findElement(By.tagName('a')).getAttribute('href');
+    console.log(attributeSignup);
 
     console.log('Successfully opened Dekhor in Firefox.');
+    await delay(3000);
   } finally {
     // Close the browser
     await driver.quit();
