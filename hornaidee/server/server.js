d42ba16e64b85d6b5352b0345a23588e83af11eb
Rookies111 <94309004+Rookies111@users.noreply.github.com>
2024-02-26 21:09:24 +0700

    Fix owner validation in PUT /dorms/:id endpoint

--- hornaidee/server/server.js
+++ hornaidee/server/server.js
@@ -176,19 +176,21 @@ app.post('/dorms', async (req, res) => {
 
 app.put('/dorms/:id', async (req, res) => {
     try {
+        req.body.owner = req.user.sub;
         const data = PutDormRequest.parse(req.body);
         const { facilities, photos, ...dormData } = data;
+        const user = req.user.sub;
 
         const { data: dormInfo, error: InfoError } = await supabase.schema('dorms').from('dorms').select('owner').eq('id', req.params.id);
-        const dormOwner = dormInfo.map(object => object.owner)[0]
         if (InfoError) {
             logger.error(InfoError);
             res.status(500).send();
             return;
         }
+        const dormOwner = dormInfo.map(object => object.owner)[0]
 
-        if (dormOwner != dormData.owner) {
-            res.status(403).json({ message: 'You are not the owner of this dorm'});
+        if (user != dormOwner) {
+            res.status(403).json({message: 'You are not the owner of this dorm'});
             return;
         }
 
