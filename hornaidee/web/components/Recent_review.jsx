03fcf99fe3ddd4000b81eab6ed210f9254e20075
krit9354 <krit9354@gmail.com>
2024-02-27 17:39:51 +0700

    creat component

---
+++ hornaidee/web/components/Recent_review.jsx
@@ -0,0 +1,15 @@
+'use clinet'
+import styles from "./CardDorm.module.css";
+import Link from 'next/link';
+import { NEXT_PUBLIC_BASE_WEB_PATH } from '../config'
+import Rating from '@mui/material/Rating';
+
+const CardDorm = (props) => {
+    return (
+        <div className={styles.dorm_card}>
+
+        </div>
+    );
+};
+
+export default CardDorm;
