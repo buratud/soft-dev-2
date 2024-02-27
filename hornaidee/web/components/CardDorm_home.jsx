c02129c42259b48f3a03e6d6f0b83bd92b4ed4cf
krit9354 <krit9354@gmail.com>
2024-02-27 17:30:16 +0700

    props

--- hornaidee/web/components/CardDorm_home.jsx
+++ hornaidee/web/components/CardDorm_home.jsx
@@ -2,17 +2,20 @@
 import styles from "./CardDorm_home.module.css";
 import Rating from '@mui/material/Rating';
 
-export default function CardDorm_home() {
+export default function CardDorm_home(props) {
+  const { img, dorm_name, address, id , star } = props;
+  console.log(star)
   return (
     <div className={styles.container}>
-        <img className={styles.img} src="https://images.sftcdn.net/images/t_app-cover-l,f_auto/p/befbcde0-9b36-11e6-95b9-00163ed833e7/260663710/the-test-fun-for-friends-screenshot.jpg"/>
+        <img className={styles.img} src={img}/>
         <div className={styles.info}>
-            <p className={styles.name}>test dorm</p>
-            <p className={styles.address}>Bangkok, TH</p>
+            <p className={styles.name}>{dorm_name}</p>
+            <p className={styles.address}>{address}</p>
             <Rating
                 size="large"
                 name="simple-controlled"
-                value={2}
+                value={star}
+                precision={0.5} 
                 readOnly
             />
         </div>
--- hornaidee/web/src/app/page.jsx
+++ hornaidee/web/src/app/page.jsx
@@ -1,11 +1,11 @@
 "use client";
 
+import CardDorm from "../../components/CardDorm";
 import CardDorm_home from "../../components/CardDorm_home";
-
 export default function Home() {
   return (
     <main>
-      <CardDorm_home></CardDorm_home>
+      empty
     </main>
   );
 }
