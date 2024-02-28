9bb0a5bea56614fdff6f98ec03b779a35e87e4e3
krit9354 <krit9354@gmail.com>
2024-02-28 20:16:02 +0700

    link

--- hornaidee/web/components/Recent_review.jsx
+++ hornaidee/web/components/Recent_review.jsx
@@ -7,24 +7,26 @@ const Recent_review = (props) => {
     const { img, dorm_name, review, id , star } = props;
     return (
         <div className={styles.card}>
-            <div className={styles.container}>
-                <img src={img} className={styles.image} />
-                <div className={styles.infoContainer}>
-                    <div className={styles.title}>{dorm_name}</div>
-                    <div className={styles.stars}>
-                        <Rating
-                        size="large"
-                        name="simple-controlled"
-                        value={star}
-                        precision={0.5}
-                        readOnly
-                        />
-                    </div>
-                    <div className={styles.description}>
-                        {review}
+            <Link href={`${NEXT_PUBLIC_BASE_WEB_PATH}/dorms/detail/${id}`}>
+                <div className={styles.container}>
+                    <img src={img} className={styles.image} />
+                    <div className={styles.infoContainer}>
+                        <div className={styles.title}>{dorm_name}</div>
+                        <div className={styles.stars}>
+                            <Rating
+                            size="large"
+                            name="simple-controlled"
+                            value={star}
+                            precision={0.5}
+                            readOnly
+                            />
+                        </div>
+                        <div className={styles.description}>
+                            {review}
+                        </div>
                     </div>
                 </div>
-            </div>
+            </Link>
         </div>
     );
 };
