d436d1186c8404c81b8fb327355df5e7b95b0009
krit9354 <krit9354@gmail.com>
2024-02-28 17:25:52 +0700

    props

--- hornaidee/web/components/Recent_review.jsx
+++ hornaidee/web/components/Recent_review.jsx
@@ -4,23 +4,24 @@ import Link from 'next/link';
 import { NEXT_PUBLIC_BASE_WEB_PATH } from '../config'
 import Rating from '@mui/material/Rating';
 const Recent_review = (props) => {
+    const { img, dorm_name, review, id , star } = props;
     return (
         <div className={styles.card}>
             <div className={styles.container}>
-                <img src="https://upload.wikimedia.org/wikipedia/commons/8/84/Sharp_Hall_Dorm_Room.jpg" className={styles.image} />
+                <img src={img} className={styles.image} />
                 <div className={styles.infoContainer}>
-                    <div className={styles.title}>Sodsai Dorm</div>
+                    <div className={styles.title}>{dorm_name}</div>
                     <div className={styles.stars}>
                         <Rating
                         size="large"
                         name="simple-controlled"
-                        value={5}
+                        value={star}
                         precision={0.5}
                         readOnly
                         />
                     </div>
                     <div className={styles.description}>
-                        "Security 24/7 with access to famous convenience store"
+                        {review}
                     </div>
                 </div>
             </div>
--- hornaidee/web/src/app/page.jsx
+++ hornaidee/web/src/app/page.jsx
@@ -6,9 +6,21 @@ import Recent_review from "../../components/Recent_review";
 export default function Home() {
   return (
     <main>
-      <div style={{padding:'100px',marginTop:'50px',display:'flex',justifyContent:'space-around'}}>
-      <Recent_review/>
-      <Recent_review/>
+      <div style={{padding:'50px',marginTop:'50px',display:'flex',justifyContent:'space-around'}}>
+      <Recent_review
+        img="https://upload.wikimedia.org/wikipedia/commons/8/84/Sharp_Hall_Dorm_Room.jpg"
+        dorm_name="test1"
+        review="so good!?"
+        id={1}
+        star="4.5"
+      />
+      <Recent_review
+        img="https://storage.googleapis.com/afs-prod/media/e7b683addae3433cbe6b800a12d8ba44/2500.jpeg"
+        dorm_name="Sodsai Dorm"
+        review="“Security 24/7 with access to famous convenience store”"
+        id={1}
+        star="5"
+      />
       </div>
       
     </main>
