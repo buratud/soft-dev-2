1157bb472a3a8e8ec21f25f34f861a22d102ba74
KeneticKills <109983145+KeneticKills@users.noreply.github.com>
2024-02-29 17:13:35 +0700

    Update server.js

--- TuachuayDekHor/server/server.js
+++ TuachuayDekHor/server/server.js
@@ -127,13 +127,6 @@ api.post("/createpost", async (req, res) => {
     const { title, category, image_link, user_id, content } = req.body;
     // const { data } = await supabase.from("blog_category").select("id").eq("category", category);
     // if (data) {
-    const { error } = await supabase.from("blog").insert({ title: title, category, body: content, cover_img: image_link, blogger: user_id })
-    if (error) {
-        res.status(500).json(error);
-    }
-    else {
-        res.status(200).json({ message: 'Create Post Success', notError: true });
-    }
         const { data, error } = await supabase.from("blog").insert({ title: title, category, body: content, cover_img: image_link, blogger: user_id })
         .select('*,blog_category(category)')
         if (error) {
