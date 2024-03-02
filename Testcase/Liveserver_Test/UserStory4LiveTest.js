ea6ee222370b68393876559742e488b3956c19da
Thanabordee <s5901012630075@email.kmutnb.ac.th>
2024-03-02 23:28:46 +0700

    Test case Userstory 4 Update User can create blog in other catagory such as recipt for cooking

--- Testcase/Liveserver_Test/UserStory4LiveTest.js
+++ Testcase/Liveserver_Test/UserStory4LiveTest.js
@@ -1,6 +1,7 @@
 const { Builder, By, Key, until } = require('selenium-webdriver');
 const firefox = require('selenium-webdriver/firefox');
 const delay = ms => new Promise(res => setTimeout(res, ms));
+const path = require('path');
 var assert = require('assert');
 const firefoxOptions = new firefox.Options().setBinary('path/to/firefox');
 
@@ -12,7 +13,7 @@ const driver = new Builder()
 
 
 // Navigate to the specified website
-(async function User_can_access_to_all_portal_and_feature() {
+(async function User_can_write_blog_in_other_catagory() {
   try {
     await driver.get('https://linux-vm-southeastasia-2.southeastasia.cloudapp.azure.com/develop');
     await delay(3000);
@@ -25,6 +26,39 @@ const driver = new Builder()
     await driver.findElement(By.tagName('button')).click();
     console.log('Fill in login info and click login.');
     await delay(2000);
+    await driver.findElement(By.className('nav_middle__WGztn')).findElement(By.xpath('//div[text()="Blogs"]')).click();
+    await delay(1000);
+    console.log('Wait for drop bar to appear');
+    await driver.findElement(By.xpath('//span[text()="Blogging"]')).click();
+    await delay(2000);
+
+    const filePath = path.join(__dirname, 'Blog2.jpeg');
+    let fileInput = await driver.findElement(By.xpath('/html/body/div[1]/div/div[1]/div/div/div/form/div[1]')).findElement(By.css('input[type="file"]'));
+    await fileInput.sendKeys(filePath);
+    console.log('Insert pic');
+    await delay(1000);
+    await driver.findElement(By.className('title__head')).findElement(By.tagName('input')).sendKeys('Tod Mun Kung');
+    await driver.findElement(By.className('Catagory')).findElement(By.id('Category')).findElement(By.xpath('//option[text()="cooking"]')).click();
+    await driver.findElement(By.className('jodit-wysiwyg')).sendKeys('รากผักชี กระเทียม พริกไทยโขลก 1 ช้อนโต๊ กุ้งสับ 250 กรัม  หมูเด้ง 150 กรัมะ แป้งมัน 2-3 ช้อนโต๊ะ  ไข่ไก่ 1 ฟอง (ใช้เฉพาะไข่แดง)     น้ำตาลทราย 1 ช้อนชา   น้ำปลา 2 ช้อนชา  ซีอิ้วขาว 1 ช้อนโต๊ะ  น้ำมันพืช สำหรับการทอด  เกล็ดขนมปัง 2 ถ้วย  น้ำจิ้มบ๊วย     น้ำจิ้มอาจาด');
+
+    await delay(1000);
+    let button = await driver.findElement(By.xpath('/html/body/div[1]/div/div[1]/div/div/div/form/div[5]/button'));
+    await delay(1000);
+    await driver.executeScript("arguments[0].scrollIntoView(true);", button);
+    await driver.wait(until.elementIsEnabled(button), 5000); // Wait up to 5 seconds
+    await delay(1000);
+    await button.click();
+    console.log('Done add blog');
+    await delay(1000);
+
+    await driver.switchTo().alert().accept();
+    console.log('Close alert');
+
+    await delay(3000);
+
+
+
+
     } finally {
       // Close the browser
       await driver.quit();
