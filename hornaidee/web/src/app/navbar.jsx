62aad4511b59c4182b81de650848e8827f673c7b
Buratud Thumboon <buratud@gmail.com>
2024-03-02 16:00:08 +0700

    Fix nav link

--- hornaidee/web/src/app/navbar.jsx
+++ hornaidee/web/src/app/navbar.jsx
@@ -162,14 +162,14 @@ export default function NavBar() {
                         {!isOpen_2 ? <span className={styles.arrow}>▼</span> : <span className={styles.arrow}>▲</span>}</button>
 
                     {isOpen_2 && <div className={styles.dropdownContent}>
-                        <Link href={`${NEXT_PUBLIC_BASE_WEB_PATH}/`}>
+                        <Link href={`${NEXT_PUBLIC_MAIN_URL}/`}>
                             <div>
                                 <span>
                                     Main
                                 </span>
                             </div>
                         </Link>
-                        <Link href={`${NEXT_PUBLIC_BASE_WEB_PATH}/all`}>
+                        <Link href={`${NEXT_PUBLIC_MAIN_URL}/all`}>
                             <div>
                                 <span>
                                     All Dorms
@@ -177,7 +177,7 @@ export default function NavBar() {
                             </div>
                         </Link>
 
-                        <Link href={`${NEXT_PUBLIC_BASE_WEB_PATH}/add`}>
+                        <Link href={`${NEXT_PUBLIC_MAIN_URL}/add`}>
                             <div>
                                 <span>
                                     Add Dorm
