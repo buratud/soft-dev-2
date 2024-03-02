c0bfef006375926714f520ef2c229bb7f4683894
Thanabordee <s5901012630075@email.kmutnb.ac.th>
2024-03-03 00:23:17 +0700

    Tese case Userstory 5 Update User can use basic fillter in dorm

--- Testcase/Liveserver_Test/UserStory5LiveTest.js
+++ Testcase/Liveserver_Test/UserStory5LiveTest.js
@@ -12,7 +12,7 @@ const driver = new Builder()
 
 
 // Navigate to the specified website
-(async function User_can_access_to_all_portal_and_feature() {
+(async function User_can_use_fillter_in_dorm() {
   try {
     await driver.get('https://linux-vm-southeastasia-2.southeastasia.cloudapp.azure.com/develop');
     await delay(3000);
@@ -25,6 +25,30 @@ const driver = new Builder()
     await driver.findElement(By.tagName('button')).click();
     console.log('Fill in login info and click login.');
     await delay(2000);
+    await driver.findElement(By.className('nav_dropdown__8bw8j')).findElement(By.xpath('//div[text()="Dorms"]')).click();
+    await delay(1000);
+    console.log('Wait for drop bar to appear');
+    await driver.findElement(By.xpath('//span[text()="Main"]')).click();
+    await delay(1000);
+    await driver.findElement(By.className('style_button__BMJeP')).click();
+    await delay(2000);
+    const actions = driver.actions({bridge: true});
+    const sliderHandle1 = await driver.findElement(By.css('span.MuiSlider-thumb:nth-child(3)'));
+    const sliderHandle2 = await driver.findElement(By.css('span.MuiSlider-thumb:nth-child(4)'));
+    await actions.dragAndDrop(sliderHandle1, {x: 50, y: 0}).perform();
+    await delay(1000);
+    console.log('Move left slide bar');
+    await actions.dragAndDrop(sliderHandle2, {x: -30, y: 0}).perform();
+    await delay(1000);
+    console.log('Move rigth slide bar');
+    await driver.findElement(By.xpath('/html/body/div[1]/div/div[2]/div[1]/div/div[1]/label[1]/input')).click();
+    console.log('click check box');
+    await delay(1000);
+    await driver.findElement(By.xpath('/html/body/div[2]/div/div[2]/div/a/div')).click();
+    console.log('select dorm after filter');
+    await delay(1000);
+
+
     } finally {
       // Close the browser
       await driver.quit();
