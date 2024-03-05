e794f14ef1172c1f880c9105833a48f9d6fc4931
Buratud Thumboon <buratud@gmail.com>
2024-03-05 16:58:38 +0700

    Revert "Add `<NoSsr>`"
    This reverts commit 5d546b163aef05a460d95abc74a0a49fbef3e603.

--- hornaidee/web/src/app/search/page.jsx
+++ hornaidee/web/src/app/search/page.jsx
@@ -1,11 +1,8 @@
 'use client'
-import NoSsr from "../../../components/NoSsr";
 import SinglePointMaps from "../../../components/SinglePointMaps/SinglePointMaps";
 
 export default function Page() {
   return (
-    <NoSsr>
-      <SinglePointMaps width="100%" height="calc(100vh - 80px)" />
-    </NoSsr>
+    <SinglePointMaps width="100%" height="calc(100vh - 80px)" />
   );
 }
