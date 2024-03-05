ff87607f576e3fe99975f0eff27da83af4713551
Buratud Thumboon <buratud@gmail.com>
2024-03-05 11:27:01 +0700

    Add search that can return distance from origin

--- hornaidee/server/server.js
+++ hornaidee/server/server.js
@@ -4,7 +4,7 @@ const bodyParser = require('body-parser')
 const { createClient } = require('@supabase/supabase-js');
 const { z } = require('zod');
 const { search } = require("./search");
-const { CreateDormRequest, CreateReviewRequest, PutReviewRequest, PutDormRequest} = require('./type');
+const { CreateDormRequest, CreateReviewRequest, PutReviewRequest, PutDormRequest } = require('./type');
 
 const { SUPABASE_URL, SUPABASE_KEY, SUPABASE_JWT_SECRET, LOG_LEVEL } = require('./config');
 const { getMimeTypeFromBase64, getFileExtensionFromMimeType, getRawBase64, isImage } = require('./util');
@@ -59,7 +59,7 @@ app.get('/recent-reviews', async (_, res) => {
 
 app.get('/dorms/search', async (req, res) => {
     try {
-        const { name: searchTerm, filter:facilityFilter, range: priceRange } = req.body;
+        const { name: searchTerm, filter: facilityFilter, range: priceRange } = req.body;
         const { data: dormsList, dormError } = await supabase.schema('dorms').from('dorms').select('id, name, rent_price, dorms_facilities(facility_id), photos(photo_url), average_stars(average)')
         dormsList.map(dorm => {
             dorm.dorms_facilities = dorm.dorms_facilities.map(facility => facility.facility_id)
@@ -83,10 +83,10 @@ app.get('/dorms/search', async (req, res) => {
         }
 
         if (dormsList.length === 0) {
-            res.json({message: 'There are no matching dorms'});
+            res.json({ message: 'There are no matching dorms' });
             return;
         }
-        
+
         for (const dorm of dormsList) {
             const facilitiesID = dorm.dorms_facilities
             if (facilityFilter == []) {
@@ -104,14 +104,14 @@ app.get('/dorms/search', async (req, res) => {
         }
 
         if (dormsList.length === 0) {
-            res.json({message: 'There are no matching dorms'});
+            res.json({ message: 'There are no matching dorms' });
             return;
         }
 
         if (searchTerm != "") {
             const result = search(searchTerm, dormsList);
             if (result.notFound) {
-                res.json({message: 'There are no matching dorms'});
+                res.json({ message: 'There are no matching dorms' });
                 return;
             }
             res.json(result);
@@ -146,6 +146,23 @@ app.get('/dorms', async (req, res) => {
             dorm.stars = average;
         }
         res.json(dorms);
+    } catch (error) {
+        logger.error(error);
+        res.status(500).send();
+    }
+});
+
+app.get('/v2/search', async (req, res) => {
+    try {
+        const { faliclites, minPrice, maxPrice, radius, longOrigin, latOrigin } = req.query;
+        const query = supabase.schema('dorms').rpc('search', { lat: Number(latOrigin), long: Number(longOrigin) });
+        const { data, error } = await query;
+        if (error) {
+            logger.error(error);
+            res.status(500).send();
+            return;
+        }
+        return res.json(data);
     } catch (error) {
         logger.error(error);
         res.status(500).send();
@@ -321,7 +338,7 @@ app.put('/dorms/:id', async (req, res) => {
         const dormOwner = dormInfo.map(object => object.owner)[0]
 
         if (req.user.sub != dormOwner) {
-            res.status(403).json({message: 'You are not the owner of this dorm'});
+            res.status(403).json({ message: 'You are not the owner of this dorm' });
             return;
         }
 
