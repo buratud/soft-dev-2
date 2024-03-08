25480acda2d3d352f215bf0efa19fa8eb3f4a508
Buratud Thumboon <buratud@gmail.com>
2024-03-08 23:50:20 +0700

    Add filter panel

--- hornaidee/web/src/app/search/page.tsx
+++ hornaidee/web/src/app/search/page.tsx
@@ -2,7 +2,7 @@
 import dynamic from "next/dynamic";
 
 const DormsSearchMaps = dynamic(() => import("../../../components/DormsSearchMaps/DormsSearchMaps"), {ssr: false});
-import './styles.scoped.scss'
+import styles from './styles.module.scss'
 import {useEffect, useState} from "react";
 import axios from "axios";
 import {NEXT_PUBLIC_AZURE_MAPS_KEY, NEXT_PUBLIC_BASE_API_URL} from "../../../config";
@@ -12,6 +12,7 @@ import React from "react";
 import {FaMagnifyingGlassLocation} from "react-icons/fa6";
 import {AzureKeyCredential} from "@azure/core-auth";
 import {KnownSearchAddressResultType, MapsSearchClient, SearchAddressResultItem} from "@azure/maps-search";
+import DormSearchResultSlider from "../../../components/DormSearchResultSlider/DormSearchResultSlider";
 
 export default function Page() {
   const client = new MapsSearchClient(new AzureKeyCredential(NEXT_PUBLIC_AZURE_MAPS_KEY));
@@ -24,7 +25,8 @@ export default function Page() {
   useEffect(() => {
     navigator.geolocation.getCurrentPosition(loc => {
       setOrigin([loc.coords.longitude, loc.coords.latitude]);
-    }, () => {});
+    }, () => {
+    });
     axios.get(`${NEXT_PUBLIC_BASE_API_URL}/v2/search?latOrigin=13.736717&longOrigin=100.523186&radius=1000`)
       .then((response) => {
           console.log(response.data);
@@ -60,40 +62,118 @@ export default function Page() {
   }
 
   return (
-    <div className="dorms-container">
-      <div className="map">
+    <div className={styles["dorms-container"]}>
+      <div className={styles["map"]}>
         <DormsSearchMaps dorms={data} origin={origin}/>
+        <div className={styles["filter-container"]}>
+          <h4>Filter</h4>
+          <p className={styles["label"]}>Price</p>
+          <DormSearchResultSlider onMinChange={() => {
+          }} onMaxChange={() => {
+          }}
+                                  maxValue={10000} minValue={0} step={1000}/>
+          <p className={styles["label"]}>Rating</p>
+          <DormSearchResultSlider onMinChange={() => {
+          }} onMaxChange={() => {
+          }}
+                                  maxValue={5} minValue={0} step={1}/>
+          <p className={styles["label"]}>Distance (Radius)</p>
+          <DormSearchResultSlider onMinChange={() => {
+          }} onMaxChange={() => {
+          }}
+                                  maxValue={20} minValue={0} step={1}/>
+          <p className={styles["label"]} style={{marginBottom: '10px'}}>Facilities</p>
+          <label className="flex items-center gap-2 ">
+            <input type="checkbox"
+                   onChange={() => {
+                   }}
+                   className="form-checkbox h-5 w-5 accent-bg-[#092F88] rounded cursor-pointer bg-[#8D8D8D]"
+            />
+            Air-Conditioner
+          </label>
+          <label className="flex items-center gap-2 ">
+            <input type="checkbox"
+                   onChange={() => {
+                   }}
+                   className="form-checkbox h-5 w-5 accent-bg-[#092F88] rounded cursor-pointer bg-[#8D8D8D]"
+            />
+            Pet-Friendly
+          </label>
+          <label className="flex items-center gap-2 ">
+            <input type="checkbox"
+                   onChange={() => {
+                   }}
+                   className="form-checkbox h-5 w-5 accent-bg-[#092F88] rounded cursor-pointer bg-[#8D8D8D]"
+            />
+            Free WiFi
+          </label>
+          <label className="flex items-center gap-2 ">
+            <input type="checkbox"
+                   onChange={() => {
+                   }}
+                   className="form-checkbox h-5 w-5 accent-bg-[#092F88] rounded cursor-pointer bg-[#8D8D8D]"
+            />
+            Elevator
+          </label>
+          <label className="flex items-center gap-2 ">
+            <input type="checkbox"
+                   onChange={() => {
+                   }}
+                   className="form-checkbox h-5 w-5 accent-bg-[#092F88] rounded cursor-pointer bg-[#8D8D8D]"
+            />
+            Near Bus Stop
+          </label>
+          <label className="flex items-center gap-2 ">
+            <input type="checkbox"
+                   onChange={() => {
+                   }}
+                   className="form-checkbox h-5 w-5 accent-bg-[#092F88] rounded cursor-pointer bg-[#8D8D8D]"
+            />
+            Near Shopping Malls
+          </label>
+          <label className="flex items-center gap-2 ">
+            <input type="checkbox"
+                   onChange={() => {
+                   }}
+                   className="form-checkbox h-5 w-5 accent-bg-[#092F88] rounded cursor-pointer bg-[#8D8D8D]"
+            />
+            Near Restaurants
+          </label>
+        </div>
         {/* Naming is hard :( */}
-        <div className="search-box-container">
-          <div className="search-bar">
-            <FaMagnifyingGlassLocation className="search-icon"/>
-            <input className="search-box" placeholder={"Search origin"} onChange={e => setSearch(e.target.value)}/>
+        <div className={styles["search-box-container"]}>
+          <div className={styles["search-bar"]}>
+            <FaMagnifyingGlassLocation className={styles["search-icon"]}/>
+            <input className={styles["search-box"]} placeholder={"Search origin"}
+                   onChange={e => setSearch(e.target.value)}/>
           </div>
-          <div className="search-result">
-            {noPoi ? <div className="search-result-ite  m">No result</div> :
+          <div className={styles["search-result"]}>
+            {noPoi ? <div className={styles["search-result-item"]}>No result</div> :
               searchResult.slice(0, 5).map((result, index) => {
                 if (result.type === KnownSearchAddressResultType.POI) {
                   return (
                     <React.Fragment key={result.id}>
-                      <div key={index} className="search-result-item" onClick={() => setOriginLocation(result)}>
+                      <div key={index} className={styles["search-result-item"]}
+                           onClick={() => setOriginLocation(result)}>
                         <p>{result.pointOfInterest.name}</p>
                         <sub>{result.address.freeformAddress}</sub>
                       </div>
-                      <div className="divider"/>
+                      <div className={styles["divider"]}/>
                     </React.Fragment>
                   )
                 }
               })}
           </div>
         </div>
+
       </div>
-      <div className="result-box">
-        <div className="filter"></div>
-        <div className="dorm-list">
+      <div className={styles["result-box"]}>
+        <div className={styles["filter"]}></div>
+        <div className={styles["dorm-list"]}>
           {data.map((dorm, index) => (
             <React.Fragment key={dorm.id}>
               <DormSearchResultCard dorm={dorm}/>
-              <div className="divider"/>
+              <div className={styles["divider"]}/>
             </React.Fragment>
           ))}
         </div>
